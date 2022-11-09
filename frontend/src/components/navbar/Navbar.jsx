79b0ee6a607cf36f9fbd5ca456e8cd1c50900d81
Pavel-Suchkou <suchkovp@gmail.com>
2022-11-09 22:42:02 +0300

    added: few components

---
+++ frontend/src/components/navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './navbar.less'
+import Logo from '../../assets/img/navbar-logo.svg'
+
+const Navbar = () => {
+    return (
+        <div className='navbar'>
+            <img src={Logo} alt="" className='navbar__logo'/>
+            <div className='navbar__header'>MERN CLOUD</div>
+            <div className="navbar__login">Enter</div>
+            <div className="navbar__registration">Registration</div>
+        </div>
+    );
+};
+
+export default Navbar;
