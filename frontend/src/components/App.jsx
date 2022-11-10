94ecf0365c4ff39191f5ca84ce26db17f3d3713e
Pavel-Suchkou <suchkovp@gmail.com>
2022-11-10 15:44:57 +0300

    added: Registration component and styles

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,12 +1,22 @@
 import Navbar from "./navbar/Navbar";
-import './app.less'
+import './app.scss'
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {Registration} from "./registration/Registration";
 
 function App() {
-  return (
-   <div className='app'>
-     <Navbar/>
-   </div>
-  );
+    return (
+        <BrowserRouter>
+            <div className='app'>
+                <Navbar/>
+                <div className="wrap">
+                    <Routes>
+                        <Route path="/registration" element={<Registration/>}/>
+                    </Routes>
+                </div>
+            </div>
+        </BrowserRouter>
+
+    );
 }
 
 export default App;
--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
-import './navbar.less'
+import './navbar.scss'
 import Logo from '../../assets/img/navbar-logo.svg'
+import {NavLink} from "react-router-dom";
 
 const Navbar = () => {
     return (
         <div className='navbar'>
-            <img src={Logo} alt="" className='navbar__logo'/>
-            <div className='navbar__header'>MERN CLOUD</div>
-            <div className="navbar__login">Enter</div>
-            <div className="navbar__registration">Registration</div>
+            <div className="container">
+                <img src={Logo} alt="" className='navbar__logo'/>
+                <div className='navbar__header'>MERN CLOUD</div>
+                <div className="navbar__login"><NavLink to="/login">Enter</NavLink></div>
+                <div className="navbar__registration"><NavLink to="/registration">Registration</NavLink></div>
+            </div>
         </div>
     );
 };
