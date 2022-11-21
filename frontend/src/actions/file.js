88070b0c37fb4432c386916c6ebf046ad0034f3e
Pavel-Suchkou <suchkovp@gmail.com>
2022-11-21 23:27:37 +0300

    upload files on front side with drag&drop was implemented

--- frontend/src/actions/file.js
+++ frontend/src/actions/file.js
@@ -2,32 +2,59 @@ import axios from "axios";
 import {addFile, setFiles} from "../reducers/fileReducer";
 
 export const getFiles = (dirId) => {
-  return async dispatch => {
-      try{
-          const response = await axios.get(`http://localhost:5010/api/files${dirId ? '?parent='+dirId : ''}`, {
-              headers: {Authorization: `Bearer ${localStorage.getItem('token')}`}
-          })
-          dispatch(setFiles(response.data))
-          console.log(response.data)
-      }catch (e) {
-          alert(e.response.data.message)
-      }
-  }
+    return async dispatch => {
+        try {
+            const response = await axios.get(`http://localhost:5010/api/files${dirId ? '?parent=' + dirId : ''}`, {
+                headers: {Authorization: `Bearer ${localStorage.getItem('token')}`}
+            })
+            dispatch(setFiles(response.data))
+            console.log(response.data)
+        } catch (e) {
+            alert(e.response.data.message)
+        }
+    }
 }
 
 export const createDir = (dirId, name) => {
     return async dispatch => {
-        try{
+        try {
             const response = await axios.post(`http://localhost:5010/api/files`, {
                 name,
                 type: 'dir',
                 parent: dirId
-            },{
+            }, {
                 headers: {Authorization: `Bearer ${localStorage.getItem('token')}`}
             })
             dispatch(addFile(response.data))
             console.log(response.data)
-        }catch (e) {
+        } catch (e) {
+            alert(e.response.data.message)
+        }
+    }
+}
+
+export const uploadFile = (file, dirId) => {
+    return async dispatch => {
+        try {
+            const formData = new FormData()
+            formData.append('file', file)
+            if (dirId) {
+                formData.append('parent', dirId)
+            }
+            const response = await axios.post(`http://localhost:5010/api/files/upload`, formData, {
+                headers: {Authorization: `Bearer ${localStorage.getItem('token')}`},
+                onUploadProgress: progressEvent => {
+                    // const totalLength = progressEvent.lengthComputable ? progressEvent.total : progressEvent.target.getResponseHeader('content-length') || progressEvent.target.getResponseHeader('x-decompressed-content-length')
+                    // console.log('total', totalLength)
+                    if (true) {
+                        let progress = Math.round((progressEvent.loaded / progressEvent.total) * 100)
+                        console.log('progress ', progress)
+                    }
+                }
+            })
+            dispatch(addFile(response.data))
+            console.log(response.data)
+        } catch (e) {
             alert(e.response.data.message)
         }
     }
--- frontend/src/components/disk/fileList/file/File.jsx
+++ frontend/src/components/disk/fileList/file/File.jsx
@@ -11,13 +11,14 @@ export const File = ({file}) => {
 
     const currentDir = useSelector(state => state.files.currentDir)
 
-    const openDirHandler = () => {
+    const openDirHandler = (file) => {
+        if(file.type === 'dir')
         dispatch(pushToStack(currentDir))
         dispatch(setCurrentDir(file._id))
     }
 
     return (
-        <div className='file' onClick={file.type === 'dir' ? () => openDirHandler() : ''}>
+        <div className='file' onClick={ () => openDirHandler(file)}>
             <img src={file.type === 'dir' ? dirLogo : fileLogo} alt="" className="file__img"/>
             <div className="file__name">{file.name}</div>
             <div className="file__date">{file.date.slice(0, 10)}</div>
